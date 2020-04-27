b2310474140ada33d418c89dfbfbd39aa8a322cd
vagrant <vagrant@localhost.localdomain>
2020-04-27 12:48:23 +0000

    復習_9章

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '89052cfd36025c9d5d4fb3c87271d7de932aa00688151f12ca13587354ff68d5fd3a04a2ecf77bc70a1e8b298bff65b036a71f4ce1581bc029d7bf048c4d8555'
